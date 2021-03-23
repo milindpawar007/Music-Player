f998420fb82fa6efc7c21efb6a87782e24546ab5
milindpawar007 <milindpawar5555@google.com>
2021-03-22 21:42:03 -0400

    next preiouse button

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
+const image= document.querySelector('img');
+const title= document.getElementById('title');
+const artist= document.getElementById('artist');
 const music=document.querySelector('audio');
 const prevBtn=document.getElementById('prev');
 const playBtn=document.getElementById('play');
 const nextBtn=document.getElementById('next');
+
+// Muisc
+const songs=[
+    {
+        name:'jacinto-1',
+        displayName:'Electric Chill Machine',
+        artist:'Jacinto Design'
+    },
+    {
+        name:'jacinto-2',
+        displayName:'Seven Nation Army',
+        artist:'Jacinto Design'
+    },
+    {
+        name:'jacinto-3',
+        displayName:'Front Row Remix',
+        artist:'Metric/Jacinto Design'
+    }
+]
 
 // check music is playing
 let isPlaying=false;
@@ -26,3 +48,21 @@ function pauseSong(){
 // Play or Pause Event Listener
 
 playBtn.addEventListener('click',()=>(isPlaying) ? pauseSong() :playSong())
+
+
+// Update  Dom
+function loadSong(song){
+    title.textContent=song.displayName;
+    artist.textContent=song.artist;
+    music.src=`music/${song.name}.mp3`;
+    image.src=`img/${song.name}.jpg`;
+}
+
+// cureent Song
+let songIndex=0;
+
+//on load -Select first song
+loadSong(songs[songIndex]);
+
+prevBtn.addEventListener('click',prevSong);
+nextBtn.addEventListener('click',nextSong);
